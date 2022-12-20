a478ccdb6ad6137d4b96436ea861fd54e8b0456b
Philip Carlsson <philipcarlsson@hotmail.com>
2022-12-20 12:15:20 +0100

    creating queryClient

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 /* import Head from "next/head";
 import Image from "next/image"; */
-import { useQuery } from "@tanstack/react-query";
+import {
+  useQuery,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const Home = () => {
-  return <p>allo</p>;
+  const queryClient = new QueryClient();
+
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <p>allo</p>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
+  );
 };
 
 export default Home;
