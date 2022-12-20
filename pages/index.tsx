9e957973eb3679218f270ab07c5a5ac8e1e29a17
Philip Carlsson <philipcarlsson@hotmail.com>
2022-12-20 12:28:39 +0100

    testing api call to products, works

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,17 +7,34 @@ import {
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const productUrl = "https://api.escuelajs.co/api/v1/products";
+
 const Home = () => {
   const queryClient = new QueryClient();
 
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <p>allo</p>
-        <ReactQueryDevtools />
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <p>allo</p>
+      <Products />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
+};
+
+const Products = () => {
+  const { data } = useQuery({
+    queryKey: ["Products"],
+    queryFn: async () => {
+      const response = await fetch(productUrl);
+      const data = await response.json();
+
+      return data;
+    },
+  });
+
+  console.log({ data });
+
+  return <div>hej</div>;
 };
 
 export default Home;
