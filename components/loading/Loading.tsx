d7806e2d1feafccaa68b34a38e8e0b4e809fd78a
Philip Carlsson <philipcarlsson@hotmail.com>
2022-12-22 17:37:58 +0100

    adding position fixed so its centered

--- components/loading/Loading.tsx
+++ components/loading/Loading.tsx
@@ -1,28 +1,29 @@
-import React, { ReactElement } from "react";
+import { ReactElement } from "react";
 import { Spinner, Text } from "@chakra-ui/react";
 import { useIsFetching } from "@tanstack/react-query";
 
 export const Loading = (): ReactElement => {
   /* returns a number of how many things are currently fetching */
-  const isFetching = true;
+  const isFetching = useIsFetching();
 
   const display = isFetching ? "display" : "none";
 
   return (
     <Spinner
+      position="fixed"
       top="50%"
       left="50%"
       transform="translate(-50%, -50%)"
       zIndex="9999"
       speed="0.7s"
       thickness="4px"
+      size="xl"
       color="#339CD8"
       emptyColor="#D2DEE5"
       role="status"
-      size="xl"
       display={display}
     >
-      <Text>Loading...</Text>
+      <Text display="none">Loading...</Text>
     </Spinner>
   );
 };
