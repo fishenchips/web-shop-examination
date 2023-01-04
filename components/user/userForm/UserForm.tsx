80f595abb35d94a93147d08b70286e8d375a95c3
Philip Carlsson <philipcarlsson@hotmail.com>
2023-01-04 10:21:02 +0100

    fixing use of refs, user can now be added to DB

--- components/user/userForm/UserForm.tsx
+++ components/user/userForm/UserForm.tsx
@@ -12,14 +12,14 @@ type Props = {
 };
 
 export const UserForm: React.FC<Props> = ({ values, onAddUser }) => {
-  const userNameRef = useRef<HTMLInputElement>();
-  const passwordRef = useRef<HTMLInputElement>();
-
-  const enteredUserName = userNameRef?.current?.value;
-  const enteredPassword = passwordRef?.current?.value;
+  const userNameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
   const handleSubmitUser = (e: SyntheticEvent) => {
     e.preventDefault();
+
+    const enteredUserName = userNameRef?.current?.value;
+    const enteredPassword = passwordRef?.current?.value;
 
     const userData = {
       userName: enteredUserName,
@@ -35,11 +35,11 @@ export const UserForm: React.FC<Props> = ({ values, onAddUser }) => {
       <h2>{values.header}</h2>
       <div>
         <label htmlFor="userName">Username</label>
-        <input type="text" name="userName" ref={enteredUserName} />
+        <input type="text" name="userName" ref={userNameRef} />
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" ref={enteredPassword} />
+        <input type="password" name="password" ref={passwordRef} />
       </div>
       <div>
         <button type="submit">{values.btnText}</button>
