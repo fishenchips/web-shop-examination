1915d2ce04d721c87bc444dc6e49636e0672df87
Philip Carlsson <philipcarlsson@hotmail.com>
2023-01-10 15:44:53 +0100

    prefetching single category on categories page

--- pages/categories/[cid]/index.tsx
+++ pages/categories/[cid]/index.tsx
@@ -1,11 +1,24 @@
 import { useRouter } from "next/router";
+import { useQueryClient } from "@tanstack/react-query";
 
 import { SingleCategory } from "../../../components/categories/SingleCategory";
+import { getCategoryById } from "../../../queries/categories/category-queries";
 
 const CategoryPage = () => {
   const router = useRouter();
 
   const cid = router.query.cid;
+
+  const usePrefetchCategoryById = async () => {
+    const queryClient = useQueryClient();
+
+    await queryClient.prefetchQuery({
+      queryKey: ["category", cid],
+      queryFn: () => getCategoryById(cid),
+    });
+  };
+
+  usePrefetchCategoryById();
 
   return <SingleCategory param={cid} />;
 };
