a96b0d2eb2a42df7dabb712400e9e7eea64a4b5d
Philip Carlsson <philipcarlsson@hotmail.com>
2023-01-14 13:15:13 +0100

    adding pagination for products

--- components/products/Products.tsx
+++ components/products/Products.tsx
@@ -1,22 +1,77 @@
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+
 import { PlatziProduct } from "../../types/product";
-import { useGetProducts } from "../../queries/products/hooks/useGetProducts";
-import styled from "./Products.module.css";
+import styles from "./Products.module.css";
 import { ProductItem } from "./ProductItem";
+import { getProducts } from "../../queries/products/product-queries";
+
+const maxProduct = 200;
 
 export const Products = () => {
-  const { data: products } = useGetProducts();
+  const [currentPage, setCurrentPage] = useState<number>(0);
+
+  const pageNumber = currentPage === 0 ? 1 : currentPage / 30 + 1;
+
+  const queryClient = useQueryClient();
+
+  const { data: products } = useQuery(
+    ["products", currentPage],
+    () => getProducts(currentPage),
+    {
+      keepPreviousData: true,
+    }
+  );
+
+  /* Prefetch next page whenever current page changes */
+  useEffect(() => {
+    if (currentPage < maxProduct) {
+      const nextPage = currentPage + 30;
+
+      queryClient.prefetchQuery(["products", nextPage], () =>
+        getProducts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
+
+  const handlePreviousPage = () => {
+    setCurrentPage((currentPage) => currentPage - 30);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((currentPage) => currentPage + 30);
+  };
 
   return (
-    <div className={styled.products}>
-      {products?.map((product: PlatziProduct) => (
-        <ProductItem
-          key={product.id}
-          id={product.id}
-          title={product.title}
-          price={product.price}
-          images={product.images}
-        />
-      ))}
-    </div>
+    <>
+      <div className={styles.products}>
+        {products?.map((product: PlatziProduct) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            images={product.images}
+          />
+        ))}
+      </div>
+      <div>
+        <button
+          className={styles.loadMore}
+          disabled={currentPage <= 0}
+          onClick={handlePreviousPage}
+        >
+          Previous Page
+        </button>
+        <p>Page: {pageNumber}</p>
+        <button
+          className={styles.loadMore}
+          disabled={currentPage >= 180}
+          onClick={handleNextPage}
+        >
+          Next Page
+        </button>
+      </div>
+    </>
   );
 };
--- queries/products/product-queries.ts
+++ queries/products/product-queries.ts
@@ -1,11 +1,9 @@
-export const getProducts = async (offset: number, limit = 30) => {
+export const getProducts = async (offset: number, limit = 30): Promise<any> => {
   const response = await fetch(
     `https://api.escuelajs.co/api/v1/products?offset=${offset}&limit=${limit}`
   );
 
-  const data = await response.json();
-
-  return data;
+  return response.json();
 };
 
 export const getProductById = async (id: number) => {
